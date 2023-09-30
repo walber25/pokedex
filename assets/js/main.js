93cd2a88aae6d14756469679b0009c30b9096783
walber25 <walberv.contatos@gmail.com>
2023-09-30 20:24:45 -0300

    Separando o consumo da API da manipulação de HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
 
 function convertPokemonToLi(pokemon){
     return `
@@ -23,17 +21,14 @@ function convertPokemonToLi(pokemon){
 }
 const pokemonList =document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        
+    pokeApi.getPokemons().then((pokemons) => {   
+        const listItens =[];
+
         for (let i = 0; i < pokemons.length; i++) {
             const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-
+            listItens.push(convertPokemonToLi(pokemon))
         }
-    
-    })
-    .catch((error) => console.error(error))
+
+        console.log(listItens);
+    })   
     
