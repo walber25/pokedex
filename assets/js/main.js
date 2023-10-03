25839b25c6008fa5da0112d481eb28e13b8ad87c
walber25 <walberv.contatos@gmail.com>
2023-10-03 09:13:32 -0300

    Inicio de mais detalhes do pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,113 @@
+// const pokemonList = document.getElementById('pokemonList');
+// const loadMoreButton = document.getElementById('loadMoreButton');
+
+// const maxRecords = 151
+// const limit = 10
+// let offset = 0;
+
+//  function loadPokemonItens(offset, limit) {
+//   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {   
+//     const newHtml = pokemons.map((pokemon) => `
+//           <li class="pokemon ${pokemon.type}">
+//                 <span class="number">#${pokemon.number}</span>
+//                 <span class="name">${pokemon.name}</span>
+                    
+//                 <div class="detail">
+//                     <ol class="types">
+//                         ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+//                     </ol>
+
+//                   <img src="${pokemon.photo}"
+//                       alt="${pokemon.name}">
+//                 </div>              
+//               </li> 
+//           `).join('')
+//     pokemonList.innerHTML += newHtml
+//  }
+
+//  )}
+
+//  loadPokemonItens(offset, limit)
+
+//  loadMoreButton.addEventListener('click', () =>{
+//     offset += limit
+//     const qtdRecordsWithNexPage = offset + limit
+   
+//     if (qtdRecordsWithNexPage >= maxRecords) {
+//       const newLimit = maxRecords - offset
+//       loadPokemonItens(offset, newLimit)
+
+//       loadMoreButton.parentElement.removeChild(loadMoreButton)
+//     }else {
+//       loadPokemonItens(offset, limit)
+//     }
+//  })
+
+
+
 const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMoreButton');
 
-const maxRecords = 151
-const limit = 10
+const maxRecords = 151;
+const limit = 10;
 let offset = 0;
 
- function loadPokemonItens(offset, limit) {
-  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {   
+function loadPokemonItens(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
     const newHtml = pokemons.map((pokemon) => `
-          <li class="pokemon ${pokemon.type}">
-                <span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
+        <li class="pokemon ${pokemon.type}">
+        <!-- redireciona para outra pagina-->
+          <a href="mais-detalhe.html"> 
+          <span class="number">#${pokemon.number}</span>
+          <span class="name">${pokemon.name}</span>
                     
-                <div class="detail">
-                    <ol class="types">
-                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                    </ol>
+          <div class="detail">
+            <ol class="types">
+               ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>           
+            
+            <img src="${pokemon.photo}" class="imagemClicavel" alt="${pokemon.name}">
+          </div>              
+        </li> 
+      `)
+      .join('');
+    
+    pokemonList.innerHTML += newHtml;
 
-                  <img src="${pokemon.photo}"
-                      alt="${pokemon.name}">
-                </div>              
-              </li> 
-          `).join('')
-    pokemonList.innerHTML += newHtml
- }
+    // // Adicione o evento de clique a cada imagem recém-criada
+    // const imagensClicaveis = document.querySelectorAll('.imagemClicavel');
+    // // imagensClicaveis.forEach((imagem) => {
+    // //   imagem.addEventListener('click', () => {
+    // //     const novaPaginaURL = 'https://www.dio.me/';
+    // //     // Abre a nova página em uma nova janela ou guia do navegador
+    // //     window.open(novaPaginaURL, '_blank');
+    // //   });
+    // // });
 
- )}
+    //   imagensClicaveis.forEach((imagem) => {
+    //   imagem.addEventListener('click', () => {
+    //     const novaPaginaURL = 'https://www.dio.me/';
 
- loadPokemonItens(offset, limit)
+    //   // Redireciona para a nova página na mesma aba
+    //     window.location.href = novaPaginaURL;
+    //   });
+    // });
 
- loadMoreButton.addEventListener('click', () =>{
-    offset += limit
-    const qtdRecordsWithNexPage = offset + limit
-   
-    if (qtdRecordsWithNexPage >= maxRecords) {
-      const newLimit = maxRecords - offset
-      loadPokemonItens(offset, newLimit)
+  });
+}
 
-      loadMoreButton.parentElement.removeChild(loadMoreButton)
-    }else {
-      loadPokemonItens(offset, limit)
-    }
- })
+loadPokemonItens(offset, limit);
+
+loadMoreButton.addEventListener('click', () => {
+  offset += limit;
+  const qtdRecordsWithNexPage = offset + limit;
+
+  if (qtdRecordsWithNexPage >= maxRecords) {
+    const newLimit = maxRecords - offset;
+    loadPokemonItens(offset, newLimit);
+
+    loadMoreButton.parentElement.removeChild(loadMoreButton);
+  } else {
+    loadPokemonItens(offset, limit);
+  }
+});
---
+++ assets/js/mais-detalhe.js
@@ -0,0 +1,32 @@
+const pokeApi = {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+return fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetails) => pokemonsDetails)
+   
+}
